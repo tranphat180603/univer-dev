a9e61297e921e0e670349be0265adcdb52b8f764
tranphat180603 <111284337+tranphat180603@users.noreply.github.com>
2024-12-13 17:12:10 +0700

    Update LLMservice.ts

--- src/LLMservice.ts
+++ src/LLMservice.ts
@@ -1,12 +1,13 @@
 import Anthropic from "@anthropic-ai/sdk";
-import Sheet from "./getSheetData";
+import { GoogleGenerativeAI } from "@google/generative-ai"
 
+// import Sheet from "./getSheetData";
 
 interface LLM{
     input: string,
 }
 
-export default class Claude implements LLM{
+export default class Gemini implements LLM{
     private _input: string
 
     constructor(){
@@ -21,27 +22,18 @@ export default class Claude implements LLM{
         this._input = input
     }
 
-    async reply(text: string = this._input): Promise<Anthropic.Messages.Message>{
-        const anthropic = new Anthropic();
-
-        const msg = await anthropic.messages.create({
-        model: "claude-3-5-sonnet-20241022",
-        max_tokens: 500,
-        temperature: 0,
-        system: "You are a helpful assistant that will help users with spreadsheet tasks such as: reading data, recognize data relations, etc.",
-        messages: [
-            {
-            "role": "user",
-            "content": [
-                {
-                "type": "text",
-                "text": text
-                }
-            ]
-            }
-        ]
-        });
-        return msg
+    async reply(text: string = this._input): Promise<string>{
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", 
+                                                systemInstruction: `You are a helpful assistant who helps user on every tasks related to working on a spreadsheet.
+                                                Your job is to complete the command that the user's ask. Basically by responding in valid JSON format and follow
+                                                the data structure on the user's spreadsheet.` });        
+        const result = await model.generateContent(text);
+        console.log(result)
+        return result.response.text()
     }
 }
 
+// const claude = new Claude()
+
+// const response = claude.reply("Tell me a funny joke about Elon Musk")
+// console.log(`Response: ${response}`)
